7c65710f5dfe240191dfba671bfea03e0f0e9334
dd <1748420480@qq.com>
2022-04-13 09:05:49 +0800

    wip

--- src/app.ts
+++ src/app.ts
@@ -8,78 +8,78 @@ const url = new URL("bricks.png", import.meta.url);
 img.src = url.pathname;
 
 function main() {
-  
-  const info_ = {
-    position: [-1,3,0,-1,-1,0,3,-1,0],
-    texcoord: [0,2,0,0,2,0],
-  };
-  const { m4 } = twgl;
-  
-  let canvas = document.querySelector("#canvas") as HTMLCanvasElement;
-  let gl = canvas.getContext("webgl2");
-  if (!gl) {
-    return;
-  }
-  const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
+    const info_ = {
+        position: [-1, 3, 0, -1, -1, 0, 3, -1, 0],
+        texcoord: [0, 2, 0, 0, 2, 0],
+    };
+    const { m4 } = twgl;
 
-  // Tell the twgl to match position with a_position, n
-  // normal with a_normal etc..
-  twgl.setAttributePrefix("a_");
+    let canvas = document.querySelector("#canvas") as HTMLCanvasElement;
+    let gl = canvas.getContext("webgl2");
+    if (!gl) {
+        return;
+    }
+    const programInfo = twgl.createProgramInfo(gl, [vs, fs]);
 
-  
-  const bufferInfo = twgl.createBufferInfoFromArrays(gl, info_);
+    // Tell the twgl to match position with a_position, n
+    // normal with a_normal etc..
+    twgl.setAttributePrefix("a_");
 
-  // const red = [1.0, 0, 0, 1];
-  const uniforms: { [key: string]: any } = {
-    // u_lightWorldPos: [0, 2.0, 15],
-    // u_lightColor: [1, 1, 1],
-    // u_ambientColor: [0.05, 0.05, 0.05],
-    // u_specularColor: [1, 1, 1],
-    // u_shininess: 40,
-    // u_color: red,
-    // u_useTexture: true,
-    // u_tex: twgl.createTexture(gl, {
-    //   min: gl.LINEAR,
-    //   mag: gl.LINEAR,
-    //   src: img,
-    // }),
-  };
+    const bufferInfo = twgl.createBufferInfoFromArrays(gl, info_);
 
-  function render(time) {
-    time *= 0.001;
-    twgl.resizeCanvasToDisplaySize(gl.canvas);
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    // const red = [1.0, 0, 0, 1];
+    const uniforms: { [key: string]: any } = {
+        u_width: canvas.width,
+        u_height: canvas.height,
+        // u_lightWorldPos: [0, 2.0, 15],
+        // u_lightColor: [1, 1, 1],
+        // u_ambientColor: [0.05, 0.05, 0.05],
+        // u_specularColor: [1, 1, 1],
+        // u_shininess: 40,
+        // u_color: red,
+        // u_useTexture: true,
+        // u_tex: twgl.createTexture(gl, {
+        //   min: gl.LINEAR,
+        //   mag: gl.LINEAR,
+        //   src: img,
+        // }),
+    };
 
-    gl.enable(gl.DEPTH_TEST);
-    gl.enable(gl.CULL_FACE);
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    function render(time) {
+        time *= 0.001;
+        twgl.resizeCanvasToDisplaySize(gl.canvas);
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-    // const fov = (30 * Math.PI) / 180;
-    // const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    // const zNear = 0.5;
-    // const zFar = 1000;
-    // const projection = m4.perspective(fov, aspect, zNear, zFar);
-    // const eye = [1, 8, 100];
-    // const target = [0, 0, 0];
-    // const up = [0, 1, 0];
+        gl.enable(gl.DEPTH_TEST);
+        gl.enable(gl.CULL_FACE);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    // const camera = m4.lookAt(eye, target, up);
-    // const view = m4.inverse(camera);
-    // const viewProjection = m4.multiply(projection, view);
-    // const world = m4.rotationY(time);
+        // const fov = (30 * Math.PI) / 180;
+        // const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
+        // const zNear = 0.5;
+        // const zFar = 1000;
+        // const projection = m4.perspective(fov, aspect, zNear, zFar);
+        // const eye = [1, 8, 100];
+        // const target = [0, 0, 0];
+        // const up = [0, 1, 0];
 
-    // uniforms.u_viewInverse = camera;
-    // uniforms.u_world = world;
-    // uniforms.u_worldInverseTranspose = m4.transpose(m4.inverse(world));
-    // uniforms.u_worldViewProjection = m4.multiply(viewProjection, world);
+        // const camera = m4.lookAt(eye, target, up);
+        // const view = m4.inverse(camera);
+        // const viewProjection = m4.multiply(projection, view);
+        // const world = m4.rotationY(time);
 
-    gl.useProgram(programInfo.program);
-    twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
-    twgl.setUniforms(programInfo, uniforms);
-    gl.drawArrays(gl.TRIANGLES, 0, bufferInfo.numElements);
+        // uniforms.u_viewInverse = camera;
+        // uniforms.u_world = world;
+        // uniforms.u_worldInverseTranspose = m4.transpose(m4.inverse(world));
+        // uniforms.u_worldViewProjection = m4.multiply(viewProjection, world);
+
+        gl.useProgram(programInfo.program);
+        twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
+        twgl.setUniforms(programInfo, uniforms);
+        gl.drawArrays(gl.TRIANGLES, 0, bufferInfo.numElements);
+        requestAnimationFrame(render);
+    }
     requestAnimationFrame(render);
-  }
-  requestAnimationFrame(render);
 }
-img.addEventListener("load", main);
-// main();
+// img.addEventListener("load", main);
+main();
--- src/f.frag
+++ src/f.frag
@@ -3,6 +3,8 @@ precision highp float;
 
 in vec4 v_position;
 in vec2 v_texCoord;
+uniform float width;//屏幕宽度
+uniform float height;//屏幕高度
 // in vec3 v_normal;
 // in vec3 v_surfaceToLight;//表面到光源的向量
 // in vec3 v_surfaceToView;//表面到相机的向量
@@ -22,10 +24,50 @@ out vec4 outColor;
 vec4 linearVertGradient(float yCoord, vec4 color_1,  vec4 color_2) {
   return mix(color_1, color_2, smoothstep(0.0,1.0 ,yCoord));
 }
+//平面上任意一点投影到法向为一个常数
+struct plane{
+  vec3 normal;
+  float c;
+};
+struct sphere{
+  vec3 center;
+  float radius;
+};
+struct ray{
+  vec3 p;//光线起点
+  vec3 d;//光线方向
+  float t;//控制光路长度
+};
+// 起始点 时间 方向
+vec3 getRay(ray r){
+return r.p+r.t*r.d;
+}
+
+//对平面求交点 
+float getRayPlaneT(ray r, plane p){
+  return (p.c-dot(r.p,p.normal))/dot(r.d,p.normal);
+}
 
 vec4 pink=vec4(1.00, 0.83, 0.83, 1.0);
 vec4 blue=vec4(0.63, 0.81, 1.00, 1.0);
+vec3 camera=vec3(0,0,-1);
+sphere ball=sphere(vec3(0,0,0),10.0);
+plane  ground=plane(vec3(0,1,0),0.0);
+float thresh=0.000001;
 void main() {
+  vec4 color;
   vec2 uv=v_texCoord;
-  outColor= linearVertGradient(uv.y,pink,blue);
+  vec3 target=vec3(uv.x*2.0-1.0,uv.y*2.0-1.0,0);
+  vec3 dir=normalize(target-camera);
+  ray r;
+  r.p=camera;
+  r.d=dir;
+  r.t=0.0;
+  if(getRayPlaneT(r,ground)>=thresh){
+    color=vec4(1.0,0,0,0);
+  }
+  else{
+    color=linearVertGradient(uv.y,pink,blue);
+  } 
+  outColor=vec4(dir,1);
 }
