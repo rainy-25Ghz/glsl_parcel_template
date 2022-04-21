1ff03f9480b008c899f28c52c4d5c8cff15519da
dd <1748420480@qq.com>
2022-04-22 01:53:13 +0800

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,25 @@
 import fs from "./f.frag";
 import vs from "./v.vert";
 import * as twgl from "../node_modules/twgl.js/dist/4.x/twgl-full";
-
-let video = document.createElement("video");
-const url = new URL("madoka.flv", import.meta.url);
+let video = document.getElementById("origin") as HTMLVideoElement;
+const url = new URL("madoka.mp4", import.meta.url);
 video.src = url.pathname;
 
+let btn=document.getElementById("btn");
+btn.onclick=()=>{
+  video.play();
+  main();
+}
 function main() {
-  const { m4 } = twgl;
+  const postArrays = {
+    position: [-1, -1, 0, 1, -1, 0, -1, 1, 0, -1, 1, 0, 1, -1, 0, 1, 1, 0],
+    texcoord: [0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 1, 1],
+  };
   // Get A WebGL context
   /** @type {HTMLCanvasElement} */
   let canvas = document.querySelector("#canvas") as HTMLCanvasElement;
   let gl = canvas.getContext("webgl2");
+
   if (!gl) {
     return;
   }
@@ -20,62 +28,41 @@ function main() {
   // Tell the twgl to match position with a_position, n
   // normal with a_normal etc..
   twgl.setAttributePrefix("a_");
-
-  const info=
   
-  const bufferInfo = twgl.createBufferInfoFromArrays(gl, info);
-
+  const bufferInfo = twgl.createBufferInfoFromArrays(gl, postArrays);
+  
   // const red = [1.0, 0, 0, 1];
   const uniforms: { [key: string]: any } = {
-    u_lightWorldPos: [0, 2.0, 15],
-    u_lightColor: [1, 1, 1],
-    u_ambientColor: [0.05, 0.05, 0.05],
-    u_specularColor: [1, 1, 1],
-    u_shininess: 40,
-    // u_color: red,
-    u_useTexture: true,
-    u_tex: twgl.createTexture(gl, {
-      min: gl.LINEAR,
-      mag: gl.LINEAR,
-      src: img,
-    }),
+    res:[canvas.width,canvas.height],
+    u_tex:null,
   };
 
   function render(time) {
-    time *= 0.001;
+    const texture= twgl.createTexture(gl, {
+      min: gl.LINEAR,
+      mag: gl.LINEAR,
+      width:gl.canvas.width,
+      height:gl.canvas.height,
+      src: null,
+    });
+    gl.bindTexture(gl.TEXTURE_2D, texture)
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, video);
+    uniforms.u_tex=texture;
+    uniforms.u_time=time*0.00000001;
     twgl.resizeCanvasToDisplaySize(gl.canvas);
     gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
     gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    const fov = (30 * Math.PI) / 180;
-    const aspect = gl.canvas.clientWidth / gl.canvas.clientHeight;
-    const zNear = 0.5;
-    const zFar = 1000;
-    const projection = m4.perspective(fov, aspect, zNear, zFar);
-    const eye = [1, 8, 100];
-    const target = [0, 0, 0];
-    const up = [0, 1, 0];
-
-    const camera = m4.lookAt(eye, target, up);
-    const view = m4.inverse(camera);
-    const viewProjection = m4.multiply(projection, view);
-    const world = m4.rotationY(time);
-
-    uniforms.u_viewInverse = camera;
-    uniforms.u_world = world;
-    uniforms.u_worldInverseTranspose = m4.transpose(m4.inverse(world));
-    uniforms.u_worldViewProjection = m4.multiply(viewProjection, world);
-
     gl.useProgram(programInfo.program);
     twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo);
     twgl.setUniforms(programInfo, uniforms);
     gl.drawArrays(gl.TRIANGLES, 0, bufferInfo.numElements);
+    gl.deleteTexture(texture);
     requestAnimationFrame(render);
   }
-  requestAnimationFrame(render);
-}
-img.addEventListener("load", main);
-// main();
+  // video.addEventListener("load", render);
+   requestAnimationFrame(render);
+} 
+
--- src/f.frag
+++ src/f.frag
@@ -1,57 +1,16 @@
 #version 300 es
 precision highp float;
 
-in vec4 v_position;
-in vec2 v_texCoord;
-in vec3 v_normal;
-in vec3 v_surfaceToLight;//表面到光源的向量
-in vec3 v_surfaceToView;//表面到相机的向量
+uniform vec2 res;
+uniform float u_time;
 
-uniform vec3 u_lightColor;//光源颜色
-uniform vec3 u_ambientColor;//环境光颜色
-uniform vec3 u_specularColor;//高光颜色
 uniform sampler2D u_tex;//纹理采样器
-uniform vec4 u_color;//不使用纹理时，设置物体颜色为纯色
-uniform bool  u_useTexture;//是否使用纹理
-uniform float u_shininess;//闪光度，代表光滑程度，越光滑，高光面积越小
 
-// uniform float u_specularFactor;//控制高光反射的系数
 
 out vec4 outColor;
 
-
-// incident 表面指向光源的向量,计算反射向量
-vec3 reflect_(vec3 incident , vec3 normal){
-  float dot_=2.0*dot(incident,normal);
-  vec3 res=dot_*normal-incident;
-  return res;
-}
-//glsl 有内置的reflect 这里练习用
-
 void main() {
-  vec3 lightDir=normalize(vec3(15,5,15));
-  vec4 objectColor =u_useTexture?texture(u_tex, v_texCoord):u_color;//计算物体本身的颜色
-  vec3 a_normal = normalize(v_normal);
-  vec3 surfaceToView = normalize(v_surfaceToView);
-
-  //计算环境光
-  vec3 ambientColor = u_ambientColor;
-  
-  // 计算漫反射
-  float cosTheta=dot(a_normal, lightDir);//求出光线与法线的夹角余弦
-  float lambert = max(cosTheta, 0.0);
-  vec3 lambertColor=lambert*objectColor.rgb;
-
-  // 计算高光
-  vec3 reflectVector = reflect(-lightDir,a_normal);//反射向量
-  float cosPhi=dot(reflectVector,surfaceToView);//计算反射光与表面到相机向量的夹角余弦
-  float specular = pow(max(cosPhi, 0.0), u_shininess);
-  vec3 specularColor=specular*u_specularColor;
-  
-  vec3 result = ambientColor+lambertColor+specularColor;
-  // result = pow(result, vec3(1.0 / 2.2) ); // gamma correction
-
-  outColor= vec4(result.x,result.y,result.z,objectColor.a);//处理透明颜色的物体
-  //我直接把其透明度保留，不参与光照计算，不知道对不对。。。
-  
+  vec2 uv= gl_FragCoord.xy/res;
+  uv=vec2(uv.x,1.0-uv.y);
+  outColor=texture(u_tex,uv);
 }
